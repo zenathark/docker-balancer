35a4feedacdeeeaba87fe345acbdbc406606cd0c
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2018-05-01 22:46:40 -0500

    Create basic HTTP client

--- client/serve.js
+++ client/serve.js
@@ -1,10 +1,19 @@
-var http = require('http');
+const http    = require('http')
+      ,Redis  = require('ioredis')
+      ,uuidv1 = require('uuid/v1')
+      ;
+
+var redis = new Redis(6379, process.env.REDIS_SERVER);
 
 function serve(ip, port) {
+    redis.set('u-id', uuidv1());
     http.createServer((req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(JSON.stringify(req.headers));
-        res.end(`\nThere's no plase like ${ip}:${port}\n`);
+        redis.get('u-id', (err, result) => {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(req.headers));
+            res.end(`\nThere's no plase like ${ip}:${port}\n 
+                     Server UUID:  ${result}`);
+        })
     }).listen(port, ip);
     console.log(`Server running at http://${ip}:${port}/`);
 }
